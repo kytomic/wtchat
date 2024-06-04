be3795e979ede6d5c0ac17c37b8c5dea3bfe5242
1 kytomic <kytomic1@gmail.com>
2024-06-04 21:50:57 +0800

    Initialize project

---
+++ src/main/java/com/dev/wtchat/WtchatApplication.java
@@ -0,0 +1,13 @@
+package com.dev.wtchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WtchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WtchatApplication.class, args);
+	}
+
+}
